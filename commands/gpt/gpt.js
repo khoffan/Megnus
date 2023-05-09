fe5aeef7a26ef8e1bcbd71cff06d75ce5ab4bf73
khoffan <khoffan222@gmail.com>
2023-05-09 23:18:16 +0700

    update 0.0.2

--- commands/gpt/gpt.js
+++ commands/gpt/gpt.js
@@ -1,10 +1,28 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+const { aiChatgpt } = require('../../components/Genarategpt');
+// const axios = require('axios');
+// const zlib = require('zlib');
+// const msg = 'hello'
+// const call = (result) => {
+//   return console.log(result)
+// }
+// aiChatgpt(msg, call)
+
+
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('gpt')
-		.setDescription('Replies answer of user'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
-	},
+  data: new SlashCommandBuilder()
+    .setName('gpt')
+    .setDescription('Replies answer of user')
+    .addStringOption(option =>
+      option
+        .setName('search')
+        .setDescription('Search for your answer')),
+  async execute(interaction) {
+    const msg = interaction.option.getString('search');
+    const call = async (result) => {
+      return await interaction.reply(result); 
+    }
+    aiChatgpt(msg, call)
+  },
 };
--- components/genarategpt.js
+++ components/genarategpt.js
@@ -1,36 +1,27 @@
-const dotenv = require('dotenv')
-dotenv.config({path:'../.env'})
+const request = require('request');
 
-const axios = require('axios');
 
-const options = {
-  method: 'POST',
-  url: 'https://openai80.p.rapidapi.com/chat/completions',
-  headers: {
-    'content-type': 'application/json',
-    'X-RapidAPI-Key': 'ae41b5ea99msh1ecd12bfa2aefeap16b035jsn3d6134450248',
-    'X-RapidAPI-Host': 'openai80.p.rapidapi.com'
-  },
-  data: {
-    model: 'gpt-3.5-turbo',
-    messages: [
-      {
-        role: 'user',
-        content: 'Hello!'
-      }
-    ]
-  }
+const aiChatgpt = (event,callback) => {
+  const text = event;
+  // console.log(text);
+  const options = {
+    method: 'POST',
+    url: 'https://openai80.p.rapidapi.com/chat/completions',
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': 'ae41b5ea99msh1ecd12bfa2aefeap16b035jsn3d6134450248',
+      'X-RapidAPI-Host': 'openai80.p.rapidapi.com'
+    },
+    body: JSON.stringify({ "model": "gpt-3.5-turbo", "messages": [{ "role": "user", "content": text }] })
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    const responseData = JSON.parse(body);
+    const aimsg = responseData.choices[0].message.content;
+    callback(aimsg)
+    // console.log(aimsg);
+  });
 };
 
-async function fetchData() {
-  try {
-    const response = await axios.request(options);
-    console.log(response.data.choice[0].messages.content);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-fetchData();
-
-  
+module.exports = { aiChatgpt }
